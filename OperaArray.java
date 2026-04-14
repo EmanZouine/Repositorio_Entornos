bb81a9a0db9d5104e33ee8ae364d7a37c4d91d8c
EmanZouine <117440289+EmanZouine@users.noreply.github.com>
2026-04-14 10:51:37 +0200

    Update OperaArray.java

--- OperaArray.java
+++ OperaArray.java
@@ -18,17 +18,16 @@ public class OperaArray {
             System.out.println(numero + ", ");
         }
     }
-
-<<<<<<< HEAD
-    public void nuevometodo1(){
-        System.out.println("Ejemplo de método 1 ");
-=======
+    
     public void sumarArry(){
         int suma = 0;
         for (int numero:numeros) {
             suma+=numero;
         }
         System.out.println("Suma de los números es de: " + suma);
->>>>>>> 6d24dd16c01b18dc596cad85a4b94391d2e2c38d
+    }
+    
+    public void nuevometodo1(){
+        System.out.println("Ejemplo de método 1 ");
     }
 }
