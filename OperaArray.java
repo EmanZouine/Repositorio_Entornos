f84a1359aa4404c53ac8f29b5549f82446e0d20b
EmanZouine <117440289+EmanZouine@users.noreply.github.com>
2026-04-14 09:40:21 +0200

    Primer commit: Añadimos fichero OperaArray.java

---
+++ OperaArray.java
@@ -0,0 +1,15 @@
+package practica.entornos.eman_zouine;
+
+/**
+ * Eman Zouine Es-Sakhi
+ */
+public class OperaArray {
+    private int[] numeros = new int[10];
+    
+    public int[] rellenaArray(){
+        for (int i = 0; i < this.numeros.length; i++) {
+            this.numeros[i] = (int)(Math.random() * 21);
+        }
+        return this.numeros;
+    }
+}
